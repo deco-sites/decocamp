be132a6aafc9728e9d5c557da86571a68038365d
Luan Lemos <ipsluan@icloud.com>
2024-03-19 18:51:28 -0300

    aula 3

---
+++ loaders/quotable.ts
@@ -0,0 +1,15 @@
+import { Quotes } from "deco-sites/decocamp/loaders/zenquotes.ts";
+
+export interface Props {
+  quantity?: number;
+}
+
+export default async function zenquotes(props: Props, _req: Request, _ctx: unknown): Promise<Quotes> {
+  const quantity = props.quantity ?? 1;
+  const promises = Array.from({ length: quantity }).map(() =>
+    fetch("https://api.quotable.io/random")
+  );
+  const responses = await Promise.all(promises);
+  const data = await Promise.all(responses.map((res) => res.json()));
+  return { data: data.map((quote) => quote.content) };
+}
---
+++ sections/BannerList.tsx
@@ -0,0 +1,31 @@
+import { Quotes } from "deco-sites/decocamp/loaders/zenquotes.ts";
+
+export interface ListItem {
+  text: string;
+  bold?: boolean;
+  color: "text-red-800" | "text-green-800";
+}
+
+export interface Props {
+  title: string;
+  description?: string;
+  items: ListItem[];
+  quote?: Quotes;
+}
+
+export default function BannerList(props: Props) {
+  console.log(props)
+
+  return (
+    <div class="bg-primary p-4">
+      <h1 class="text-2xl font-bold">{props.title}</h1>
+      {props.description && <p class="text-gray-600">{props.description}</p>}
+      <ul class="mt-4">
+        {props.items.map((item, index) => (
+          <li key={index} class={`${item.color ?? "text-gray-800"}`}>{item.text}</li>
+        ))}
+      </ul>
+      {props.quote && <p>{props.quote.data}</p>}
+    </div>
+  )
+}
